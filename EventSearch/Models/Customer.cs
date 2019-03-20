2c3a35f797736b2680612ebd5ebbc1f51e72a70c
Bill Lor <blorwi@gmail.com>
2019-03-20 12:24:49 -0500

    added search and geocode mdoels

--- EventSearch/Models/Customer.cs
+++ EventSearch/Models/Customer.cs
@@ -7,5 +7,22 @@ namespace EventSearch.Models
 {
     public class Customer
     {
+        public Dictionary<int, string> cuisines = new Dictionary<int, string>() { };
+
+        public Dictionary<int, string> restaurants = new Dictionary<int, string>() { };
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double entity_id { get; set; }
+        public string entity_type { get; set; }
+        public double city_id { get; set; }
+        public double cuisine_id { get; set; }
+        public string cuisine_name { get; set; }
+        public double restaurant_id { get; set; }
+        public string RestaurantName { get; set; }
+        public string Address { get; set; }
+        public string PhotoURL { get; set; }
+        public double CostForTwo { get; set; }
+        public string ReviewText { get; set; }
     }
 }
